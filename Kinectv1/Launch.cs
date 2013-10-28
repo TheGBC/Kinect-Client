1824cf088560a62b63839c40b2b1e6626dea162c
yuhao <yuhao@google.com>
2013-10-27 22:24:43 -0700

    Connected components to make http post request to a url every second with most recent depth frame

--- Kinectv1/Launch.cs
+++ Kinectv1/Launch.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Kinectv1 {
   class Launch {
+    static string id = "1";
     static void Main(string[] args) {
+      Thread thread = new Thread(Run);
+      thread.Start();
       Wait();
     }
 
@@ -15,6 +19,21 @@ namespace Kinectv1 {
     /// </summary>
     static void Wait() {
       while (true) ;
+    }
+
+    static void Run() {
+      KinectManager manager = new KinectManager();
+      Dictionary<string, string> map = new Dictionary<string,string>();
+      map["id"] = id;
+      while (true) {
+        Thread.Sleep(1000);
+        string data = manager.SerializeCurrentFrame();
+        if (data != null) {
+          map["data"] = data;
+          NetworkDispatcher.SynchronizedPost(map, "");
+          Console.WriteLine("Sent frame");
+        }
+      }
     }
   }
 }
